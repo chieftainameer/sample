211b01e07edb9a9bb88e9d821516e1b80ca92522
chieftainameer <chieftainameer@gmail.com>
2018-11-21 01:01:21 +0500

    finsih user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+#we can use options = {size: 80}
+
+	def gravatar_for(user, size: 30)
+		gravatar_id=Digest::MD5::hexdigest(user.email.downcase)
+	# size=options[:size]
+		gravatar_url="https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar" )
+		
+		
+	end
 end
